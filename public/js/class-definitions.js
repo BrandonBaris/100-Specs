911746c1059dc756e541d74244c27cc9184c9a0c
Brandon Baris <brandonbaris@gmail.com>
2015-02-04 11:06:56 -1000

    adds isWarmBlooded methods being called on Animal instances

--- public/js/class-definitions.js
+++ public/js/class-definitions.js
@@ -1313,9 +1313,9 @@ Meal.prototype.containsJunkFood = function(){
  * and assign the values to each variable below.
  *
  */
-var warmBloodedAnimal;
-var coldBloodedAnimal;
-var notWarmOrColdAnimal;
+var warmBloodedAnimal = new Animal('Bird').isWarmBlooded();
+var coldBloodedAnimal = new Animal('Fish').isWarmBlooded();
+var notWarmOrColdAnimal = new Animal('Brandon').isWarmBlooded();
 
 
 /* Step 92
